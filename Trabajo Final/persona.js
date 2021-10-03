154d5a5743de964f5f8eb8a8f65e430ff4cf1693
dlagardo <darioalbertolagardo@hotmail.com>
2021-10-03 11:54:14 -0300

    c

--- Trabajo Final/persona.js
+++ Trabajo Final/persona.js
@@ -7,5 +7,5 @@ const personas = [];
 const persona1 = new Persona(prompt("Ingrese su nombre"));
 personas.push(persona1.nombre)
 
-let text = document.createTextNode('Bienvenido : ' + persona1.nombre);
+let text = document.createTextNode('Bienvenido  ' + persona1.nombre);
 document.getElementById("bienvenido").appendChild(text);
