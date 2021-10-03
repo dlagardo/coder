2cb0d00da513df25bc40f060a2de34102b08486e
dlagardo <darioalbertolagardo@hotmail.com>
2021-10-03 15:14:12 -0300

    c

--- Trabajo Final/Producto.js
+++ Trabajo Final/Producto.js
@@ -1,74 +1,74 @@
 
-let b = 0;
-let i = 1;
+let suma_Prod = 0;
+let cant_prod = 1;
 let sumatotal = 0;
 
 function agregar() {
   let a = document.getElementById('1').innerHTML;
   a = parseFloat(a);
 
-  b = b + a;//suma las veces que se selecciona el producto
+  suma_Prod = suma_Prod + a;//suma las veces que se selecciona el producto
   sumatotal = sumatotal + a;//variable Global para sumar los distintos
 
   document.getElementById("precio-boca").innerHTML += '<h7>' + " " + '</h7>';
-  document.getElementById("precio-boca").innerHTML = '<h7>' + '$' + b + '</h7>';
+  document.getElementById("precio-boca").innerHTML = '<h7>' + '$' + suma_Prod + '</h7>';
   document.getElementById("boca").innerHTML = '<h7>Pelota de Boca</h7>';
-  document.getElementById("cant-boca").innerHTML = '<h7>' + i + '</h7>';
+  document.getElementById("cant-boca").innerHTML = '<h7>' + cant_prod+ '</h7>';
   document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
 
-  i++;//variable para sumar cantidad de productos agregados al carro
+  cant_prod++;//variable para sumar cantidad de productos agregados al carro
 };
-let c = 0;
-let d = 1;
+let suma_Prod2 = 0;
+let cant_prod2= 1;
 function agregar2() {
   let a = document.getElementById('2').innerHTML;
   a = parseFloat(a);
 
-  c = c + a;//suma las veces que se selecciona el producto
+  suma_Prod2 = suma_Prod2 + a;//suma las veces que se selecciona el producto
   sumatotal = sumatotal + a;//variable Global para sumar los distintos
 
   document.getElementById("precio-river").innerHTML += '<h7>' + " " + '</h7>';
-  document.getElementById("precio-river").innerHTML = '<h7>' + '$' + c + '</h7>';
+  document.getElementById("precio-river").innerHTML = '<h7>' + '$' + suma_Prod2 + '</h7>';
   document.getElementById("river").innerHTML = '<h7>Pelota de river</h7>';
-  document.getElementById("cant-river").innerHTML = '<h7>' + d + '</h7>';
+  document.getElementById("cant-river").innerHTML = '<h7>' + cant_prod2 + '</h7>';
   document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
 
-  d++;//variable para sumar cantidad de productos agregados al carro
+  cant_prod2++;//variable para sumar cantidad de productos agregados al carro
 
 }
-
-let x = 0;
-let e = 1;
+let suma_Prod3 = 0;
+let cant_prod3= 1;
 function agregar3() {
   let a = document.getElementById('3').innerHTML;
   a = parseFloat(a);
 
-  x = x + a;//suma las veces que se selecciona el producto
+  suma_Prod3 = suma_Prod2 + a;//suma las veces que se selecciona el producto
   sumatotal = sumatotal + a;//variable Global para sumar los distintos
  
   document.getElementById("precio-sanlorenzo").innerHTML += '<h7>' + " " + '</h7>';
-  document.getElementById("precio-sanlorenzo").innerHTML = '<h7>' + '$' + x + '</h7>';
+  document.getElementById("precio-sanlorenzo").innerHTML = '<h7>' + '$' + suma_Prod3 + '</h7>';
   document.getElementById("sanlorenzo").innerHTML = '<h7>Pelota de San Lorenzo</h7>';
-  document.getElementById("cant-sanlorenzo").innerHTML = '<h7>' + e + '</h7>';
+  document.getElementById("cant-sanlorenzo").innerHTML = '<h7>' + cant_prod3+ '</h7>';
   document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
 
-  e++;//variable para sumar cantidad de productos agregados al carro
+  cant_prod3++;//variable para sumar cantidad de productos agregados al carro
 };
-let z = 0;
-let f = 1;
+let suma_Prod4 = 0;
+let cant_prod4= 1;
+
 function agregar4() {
   let a = document.getElementById('4').innerHTML;
   a = parseFloat(a);
 
-  z = z + a;//suma las veces que se selecciona el producto
+  suma_Prod4 = suma_Prod4+ a;//suma las veces que se selecciona el producto
   sumatotal = sumatotal + a;//variable Global para sumar los distintos
 
   document.getElementById("precio-indep").innerHTML += '<h7>' + " " + '</h7>';
-  document.getElementById("precio-indep").innerHTML = '<h7>' + '$' + z + '</h7>';
+  document.getElementById("precio-indep").innerHTML = '<h7>' + '$' + suma_Prod4 + '</h7>';
   document.getElementById("indep").innerHTML = '<h7>Pelota de Independiente</h7>';
-  document.getElementById("cant-indep").innerHTML = '<h7>' + f + '</h7>';
+  document.getElementById("cant-indep").innerHTML = '<h7>' + cant_prod4 + '</h7>';
   document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
-  f++;//variable para sumar cantidad de productos agregados al carro
+  cant_prod4++;//variable para sumar cantidad de productos agregados al carro
 
 };
 btn1.addEventListener('click', agregar);
