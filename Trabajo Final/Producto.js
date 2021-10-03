d4604df9fd4acf5699675fd210a030025cfdd389
dlagardo <darioalbertolagardo@hotmail.com>
2021-10-03 11:33:56 -0300

    c

--- Trabajo Final/Producto.js
+++ Trabajo Final/Producto.js
@@ -1,91 +1,78 @@
 
-let b=0;
-let i =1;
-let sumatotal=0;
+let b = 0;
+let i = 1;
+let sumatotal = 0;
 
-function agregar(){
-    var a = document.getElementById('1').innerHTML;
-    var a=parseFloat(a);
+function agregar() {
+  let a = document.getElementById('1').innerHTML;
+  a = parseFloat(a);
 
-    b=b+a;
-    sumatotal=sumatotal+a;
+  b = b + a;//suma las veces que se selecciona el producto
+  sumatotal = sumatotal + a;//variable Global para sumar los distintos
 
-  document.getElementById("precio-boca").innerHTML+= '<h7>'+" "+'</h7>';
-  document.getElementById("precio-boca").innerHTML= '<h7>'+'$'+b+'</h7>';
-  document.getElementById("boca").innerHTML= '<h7>Pelota de Boca</h7>';
-  document.getElementById("cant-boca").innerHTML= '<h7>'+i+'</h7>';
-  document.getElementById("total").innerHTML= '<h7>'+parseInt(sumatotal)+'</h7>';
+  document.getElementById("precio-boca").innerHTML += '<h7>' + " " + '</h7>';
+  document.getElementById("precio-boca").innerHTML = '<h7>' + '$' + b + '</h7>';
+  document.getElementById("boca").innerHTML = '<h7>Pelota de Boca</h7>';
+  document.getElementById("cant-boca").innerHTML = '<h7>' + i + '</h7>';
+  document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
 
- i++;
-
-
-  
+  i++;
 };
-let c=0;
-let d =1;
-function agregar2(){
-    var a = document.getElementById('2').innerHTML;
-    var a=parseFloat(a);
-   
-   c=c+a; 
-   sumatotal=sumatotal+a;
-  
-//   baseDatos.push(nuevaVenta);
-//   console.log(baseDatos);
+let c = 0;
+let d = 1;
+function agregar2() {
+  let a = document.getElementById('2').innerHTML;
+  a = parseFloat(a);
 
-  document.getElementById("precio-river").innerHTML+= '<h7>'+" "+'</h7>';
-  document.getElementById("precio-river").innerHTML= '<h7>'+'$'+c+'</h7>';
-  document.getElementById("river").innerHTML= '<h7>Pelota de river</h7>';
-  document.getElementById("cant-river").innerHTML= '<h7>'+d+'</h7>';
-  document.getElementById("total").innerHTML= '<h7>'+parseInt(sumatotal)+'</h7>';
+  c = c + a;//suma las veces que se selecciona el producto
+  sumatotal = sumatotal + a;//variable Global para sumar los distintos
 
- d++;
- 
+  document.getElementById("precio-river").innerHTML += '<h7>' + " " + '</h7>';
+  document.getElementById("precio-river").innerHTML = '<h7>' + '$' + c + '</h7>';
+  document.getElementById("river").innerHTML = '<h7>Pelota de river</h7>';
+  document.getElementById("cant-river").innerHTML = '<h7>' + d + '</h7>';
+  document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
+
+  d++;
+
 }
 
-let x=0;
-let e =1;
-function agregar3(){
-    var a = document.getElementById('3').innerHTML;
-    var a=parseFloat(a);
-   
-   x=x+a;
-   sumatotal=sumatotal+a;
-//   baseDatos.push(nuevaVenta);
-//   console.log(baseDatos);
+let x = 0;
+let e = 1;
+function agregar3() {
+  let a = document.getElementById('3').innerHTML;
+  a = parseFloat(a);
 
-  document.getElementById("precio-sanlorenzo").innerHTML+= '<h7>'+" "+'</h7>';
-  document.getElementById("precio-sanlorenzo").innerHTML= '<h7>'+'$'+x+'</h7>';
-  document.getElementById("sanlorenzo").innerHTML= '<h7>Pelota de San Lorenzo</h7>';
-  document.getElementById("cant-sanlorenzo").innerHTML= '<h7>'+e+'</h7>';
-  document.getElementById("total").innerHTML= '<h7>'+parseInt(sumatotal)+'</h7>';
+  x = x + a;//suma las veces que se selecciona el producto
+  sumatotal = sumatotal + a;//variable Global para sumar los distintos
+ 
+  document.getElementById("precio-sanlorenzo").innerHTML += '<h7>' + " " + '</h7>';
+  document.getElementById("precio-sanlorenzo").innerHTML = '<h7>' + '$' + x + '</h7>';
+  document.getElementById("sanlorenzo").innerHTML = '<h7>Pelota de San Lorenzo</h7>';
+  document.getElementById("cant-sanlorenzo").innerHTML = '<h7>' + e + '</h7>';
+  document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
 
-
- e++;
-
-//   document.getElementById("cant-boca").innerHTML+= '<h7>'+baseDatos.length+'</h7>';
+  e++;
 };
-let z=0;
-let f =1;
-function agregar4(){
-    var a = document.getElementById('4').innerHTML;
-    var a=parseFloat(a);
-   
-   z=z+a;
-   sumatotal=sumatotal+a;
-//   baseDatos.push(nuevaVenta);
-//   console.log(baseDatos);
+let z = 0;
+let f = 1;
+function agregar4() {
+  let a = document.getElementById('4').innerHTML;
+  a = parseFloat(a);
 
-  document.getElementById("precio-indep").innerHTML+= '<h7>'+" "+'</h7>';
-  document.getElementById("precio-indep").innerHTML= '<h7>'+'$'+z+'</h7>';
-  document.getElementById("indep").innerHTML= '<h7>Pelota de Independiente</h7>';
-  document.getElementById("cant-indep").innerHTML= '<h7>'+f+'</h7>';
-  document.getElementById("total").innerHTML= '<h7>'+parseInt(sumatotal)+'</h7>';
- f++;
-//   document.getElementById("cant-boca").innerHTML+= '<h7>'+baseDatos.length+'</h7>';
+  z = z + a;//suma las veces que se selecciona el producto
+  sumatotal = sumatotal + a;//variable Global para sumar los distintos
+
+  document.getElementById("precio-indep").innerHTML += '<h7>' + " " + '</h7>';
+  document.getElementById("precio-indep").innerHTML = '<h7>' + '$' + z + '</h7>';
+  document.getElementById("indep").innerHTML = '<h7>Pelota de Independiente</h7>';
+  document.getElementById("cant-indep").innerHTML = '<h7>' + f + '</h7>';
+  document.getElementById("total").innerHTML = '<h7>' + parseInt(sumatotal) + '</h7>';
+  f++;
+
 };
-btn1.addEventListener('click',agregar);
-btn2.addEventListener('click',agregar2);
-btn3.addEventListener('click',agregar3);
-btn4.addEventListener('click',agregar4);
+btn1.addEventListener('click', agregar);
+btn2.addEventListener('click', agregar2);
+btn3.addEventListener('click', agregar3);
+btn4.addEventListener('click', agregar4);
 
--- Trabajo Final/persona.js
+++ Trabajo Final/persona.js
@@ -1,23 +1,11 @@
-class Persona{
-constructor(nombre){
- this.nombre=nombre;
+class Persona {
+    constructor(nombre) {
+        this.nombre = nombre;
+    }
 }
-}
-const personas=[];
-const persona1=new Persona('dario');
-// personas.push(persona1);
-// const nombre=document.createElement('h6');
-// nombre.innerText=personas.nombre;
+const personas = [];
+const persona1 = new Persona(prompt("Ingrese su nombre"));
+personas.push(persona1.nombre)
 
-
-var text = document.createTextNode('Bienvenido : '+prompt("Ingrese su nombre"));   
-document.getElementById("bienvenido").appendChild(text); 
-//document.getElementById("bienvenido").innerHTML = `Gracias por su visita ${personas[0].nombre} `;
-
-// const nombre=document.createElement('h6');
-// nombre.innerText=personas.nombre;
-// contenedor.appendChild(nombre);
-// const estilo=document.createElement('p');
-// estilo.innerText=`Gracias por su visita ${personas[0].nombre} `;
-// contenedor.appendChild(estilo);
-// document.body.appendChild(contenedor);
+let text = document.createTextNode('Bienvenido : ' + persona1.nombre);
+document.getElementById("bienvenido").appendChild(text);
